4c81b9adf6e7d5d7b1a5be244dcb9dcc8fb7a615
Aidan Ayensu <aidanayensu@gmail.com>
2023-08-04 11:02:44 +0100

    Updated match_maker.py with improved pairing loop

--- match_maker.py
+++ match_maker.py
@@ -1,8 +1,11 @@
+import time
 from sentence_transformers import SentenceTransformer, util
 import torch
 from torch.utils.data import DataLoader, Dataset
+import torch.nn as nn
 import json
 from cleanco import basename
+import pandas as pd
 
 def initialise_match_maker(model='distilbert-base-nli-stsb-mean-tokens', threshold=0.8, batch_size=64, device='cpu'):
     """ Wrapper to select encoder model. """
@@ -36,6 +39,8 @@ class MatchMaker:
         self.threshold = threshold
         self.batchsize = batch_size
         self.device = torch.device(device)
+        if device == 'mps':
+            self.model = nn.DataParallel(self.model)
 
     def similarity(self, embeddings):
         """ Calculate similarity based on cosine. """
@@ -45,27 +50,40 @@ class MatchMaker:
         """ Load each batch and create embeddings of names. """
         dataloader = DataLoader(names, batch_size=self.batchsize, shuffle=False)
 
-        embeddings = [self.model.encode(batch, convert_to_tensor=True) for batch in dataloader]
+        if self.device == 'mps':
+            embeddings = [self.model.module.encode(batch, convert_to_tensor=True) for batch in dataloader]
+        else:
+            embeddings = [self.model.encode(batch, convert_to_tensor=True) for batch in dataloader]
         embeddings = torch.cat(embeddings, dim=0).to(self.device)
         return embeddings
     
     def pair(self, names):
         """ Create list of pairs from embeddings. """
+        emb_start = time.time()
         embeddings = self.create_embeddings(names)
+        sim_start = time.time()
         similarity = self.similarity(embeddings)
+        pair_start = time.time()
 
         pairs = set()
         for i in range(len(names)):
-            for j in range(i+1, len(names)):
-                if similarity[i][j] >= self.threshold:
+            idxs = torch.where(similarity[i] >= self.threshold)[0]
+            for j in idxs:
+                if j != i:
                     pair = tuple(sorted((names[i], names[j])))
                     pairs.add(pair)
+        
+        pair_end = time.time()
+        
+        print(f"Embedding time: {(sim_start-emb_start)}")
+        print(f"Scoring time: {(pair_start-sim_start)}")
+        print(f"Pairing time: {(pair_end-pair_start)}")
 
         return pairs
     
 if __name__ == '__main__':
     dataset = OrgNamesDataset('org_names.json', transform=basename)
-    subset_names = dataset.names[:10]
-    match_maker = initialise_match_maker(model='distilbert-base-nli-stsb-mean-tokens', threshold=0.8, batch_size=64, device='cpu')
+    match_maker = initialise_match_maker(model='distilbert-base-nli-stsb-mean-tokens', threshold=0.8, batch_size=10000, device='cpu')
     pairs = match_maker.pair(dataset)
+    pairs_df = pd.DataFrame(pairs, columns=['Organisation', 'Related Organisation'])
     print(pairs)
